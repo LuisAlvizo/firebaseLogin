b01819cb421374b3c06de91ca8172d81792b4842
Alvizo07 <al291339@edu.uaa.mx>
2023-05-29 01:57:09 -0600

    ya me quiero mimir

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
     production: false,
     firebaseConfig: {
-        apiKey: "AIzaSyBF6KZLlf5ewEULFpykVw6mTTu6vKm_8rY",
-        authDomain: "fir-login-60619.firebaseapp.com",
-        projectId: "fir-login-60619",
-        storageBucket: "fir-login-60619.appspot.com",
-        messagingSenderId: "719132982087",
-        appId: "1:719132982087:web:58b23464b829a80a0daa62"
+        apiKey: "AIzaSyA92RfCCXRKxGeYiRi0K_0nGg_M4hyqFZQ",
+        authDomain: "fir-2-df271.firebaseapp.com",
+        projectId: "fir-2-df271",
+        storageBucket: "fir-2-df271.appspot.com",
+        messagingSenderId: "475452440682",
+        appId: "1:475452440682:web:95f534bb5bbb0b1a320a13"
     }
 }
