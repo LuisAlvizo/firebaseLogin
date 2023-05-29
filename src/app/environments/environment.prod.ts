ac8756150957d1df2b8161ccfd72a1157bbb1d6e
Alvizo07 <al291339@edu.uaa.mx>
2023-05-29 00:25:53 -0600

    cambio

---
+++ src/app/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false,
+    firebaseConfig: {
+        apiKey: "AIzaSyBF6KZLlf5ewEULFpykVw6mTTu6vKm_8rY",
+        authDomain: "fir-login-60619.firebaseapp.com",
+        projectId: "fir-login-60619",
+        storageBucket: "fir-login-60619.appspot.com",
+        messagingSenderId: "719132982087",
+        appId: "1:719132982087:web:58b23464b829a80a0daa62"
+    }
+}
